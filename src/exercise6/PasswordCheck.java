82de4dab7cf1349063941e219e28fca18c019df6
MartoETuk <martin_shishkovski@abv.bg>
2020-11-04 18:50:26 +0000

    purtvi opit

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,27 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter password: ");
+        String password = input.nextLine();
+        boolean check = true;
+        int counter = 0;
+
+        for (int index = 0; index < password.length(); index++) {
+          if (!Character.isLetterOrDigit(password.charAt(index))) {
+            check = false;
+            break;
+          }
+
+          if (Character.isDigit(password.charAt(index))) {
+            counter++;
+          } 
+        }
+
+        if (password.length() >= 8 && counter >= 2 && check == true) {
+          System.out.println("Password valid!");
+        } else {
+          System.out.println("Passwword invalid");
+        }
     }
 }
